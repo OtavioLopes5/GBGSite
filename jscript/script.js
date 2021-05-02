afe3fea5307214cb2b2a00cdae90eb58e6324b64
Otavio Lopes <60718044+OtavioLopes5@users.noreply.github.com>
2021-05-02 12:40:30 -0400

    Add files via upload

---
+++ jscript/script.js
@@ -0,0 +1,16 @@
+function onOff() {
+    document
+    .querySelector("#modal")
+    .classList
+    .toggle("hide")
+
+    document
+    .querySelector("body")
+    .classList
+    .toggle("hideScrool")
+
+    document
+    .querySelector("#modal")
+    .classList
+    .toggle("addScrool")
+}
